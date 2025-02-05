2be60b03289ebe3b59ab52774a34e83ae1a73623
Subhan <subhan.suleman@whetstonez.com>
2025-02-05 14:35:56 +0500

    Changes in RCN

--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -1,62 +1,70 @@
 import React from "react";
 
-const EcoSystem = () => {
+const RCN = () => {
   return (
-    <div className="bg-[#EDF7FF]  w-[100%] font-bold flex flex-col">
-      <div className="text-[#005FA6] text-[34px] px-[3%] py-[2%]">
-        Equinix Jeddah Internet Exchange
-      </div>
-      <div
-        className="text-[#005FA6] w-[290px] h-[70px] bg-[#FFFFFF] text-[24px] font-semibold 
-                flex items-center justify-center mb-12 mx-[3%] "
-      >
-        Exchange Ecosystem
-      </div>
-      <div className="flex justify-center gap-4">
-        <div className="grid grid-cols-3 gap-4 self-baseline">
-          <div className="  ">
-            <img
-              src="/pages/ecosystem/content.svg"
-              className=""
-              width={"250px"}
-            />
-          </div>
-          <div className=" ">
-            <img
-              src="/pages/ecosystem/network.svg"
-              className=""
-              width={"250px"}
-            />
-          </div>
-          <div className=" ">
-            <img
-              src="/pages/ecosystem/cloud.svg"
-              className=""
-              width={"250px"}
-            />
-          </div>
+    <div className="w-full grid grid-cols-3">
+      <div className="w-full col-span-1 gap-10 px-[4vw] pt-[4vw]">
+        <div>
+          <p className="text-[#005FA6] text-[32px] font-[700]">RCN</p>
+        </div>
+        <div className="grid gap-6">
+          <p className="text-[#001424] text-[18px] font-[400]">
+            The RCN Cable was established <br /> through an agreement in 2010,
+            <br /> is a Terrestrial Cable that <br />
+            connects the United Arab
+            <br /> Emirates (UAE), Kingdom of
+            <br /> Saudi Arabia (KSA), Jordan, Syria,
+            <br /> and Turkey.
+          </p>
+          <p className="text-[#001424] text-[18px] font-[400]">
+            In 2015, the segments of the
+            <br /> cable in the UAE, KSA, and
+            <br /> Jordan were partially put into
+            <br /> service.
+          </p>
         </div>
       </div>
-      <div className="flex justify-center">
-        <div className="grid grid-cols-2 gap-4">
-          <div>
-            <img
-              src="/pages/ecosystem/enterprise.svg"
-              className=""
-              width={"250px"}
-            />
-          </div>
-          <div className=" ">
-            <img
-              src="/pages/ecosystem/financial.svg"
-              className=""
-              width={"250px"}
-            />
-          </div>
+      {/* Image Positioned at Bottom-Right */}
+      <div className="col-span-2 ">
+        <div className="flex justify-end">
+          <img
+            src="/pages/rcnmap.svg"
+            className="max-w-[100%] md:max-w-[100%] lg:max-w-[100%] xl:max-w-[100%] h-[100vh] "
+          />
         </div>
       </div>
     </div>
+    // <div className="w-full relative h-[100vh]">
+    //   <div className="w-full flex flex-col gap-10 px-[4vw] pt-[4vw]">
+    //     <div>
+    //       <p className="text-[#005FA6] text-[32px] font-[700]">RCN</p>
+    //     </div>
+    //     <div className="grid gap-6">
+    //       <p className="text-[#001424] text-[18px] font-[400]">
+    //         The RCN Cable was established <br /> through an agreement in 2010,
+    //         <br /> is a Terrestrial Cable that <br />
+    //         connects the United Arab
+    //         <br /> Emirates (UAE), Kingdom of
+    //         <br /> Saudi Arabia (KSA), Jordan, Syria,
+    //         <br /> and Turkey.
+    //       </p>
+    //       <p className="text-[#001424] text-[18px] font-[400]">
+    //         In 2015, the segments of the
+    //         <br /> cable in the UAE, KSA, and
+    //         <br /> Jordan were partially put into
+    //         <br /> service.
+    //       </p>
+    //     </div>
+    //   </div>
+    //   {/* Image Positioned at Bottom-Right */}
+    //   <div className="absolute top-0 right-0 h-[100vh]">
+    //     <img
+    //       src="/pages/rcnmap.svg"
+    //       className="max-w-[100%] md:max-w-[100%] lg:max-w-[100%] xl:max-w-[100%] h-[100vh] "
+    //     />
+    //   </div>
+    // </div>
   );
 };
 
-export default EcoSystem;
+export default RCN;
--- src/pages/view/RCN.jsx
+++ src/pages/view/RCN.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const RCN = () => {
   return (
-    <div>
-      <div className="absolute flex flex-col gap-10 px-[4vw] pt-[6vw] ">
+    <div className="w-full grid grid-cols-3">
+      <div className="w-full col-span-1 gap-10 px-[4vw] pt-[4vw]">
         <div>
-          <p className="text-[#005FA6] text-[32px] font-[700] ">RCN</p>
+          <p className="text-[#005FA6] text-[32px] font-[700]">RCN</p>
         </div>
-        <div className="grid gap-6 ">
-          <p className="text-[#001424] text-[18px] font-[400] ">
+        <div className="grid gap-6">
+          <p className="text-[#001424] text-[18px] font-[400]">
             The RCN Cable was established <br /> through an agreement in 2010,
             <br /> is a Terrestrial Cable that <br />
             connects the United Arab
@@ -16,7 +16,7 @@ const RCN = () => {
             <br /> Saudi Arabia (KSA), Jordan, Syria,
             <br /> and Turkey.
           </p>
-          <p className="text-[#001424] text-[18px] font-[400] ">
+          <p className="text-[#001424] text-[18px] font-[400]">
             In 2015, the segments of the
             <br /> cable in the UAE, KSA, and
             <br /> Jordan were partially put into
@@ -24,10 +24,46 @@ const RCN = () => {
           </p>
         </div>
       </div>
-      <div className=" flex justify-end">
-        <img src="/pages/rcnmap.svg" width={"41.7%"} />
+      {/* Image Positioned at Bottom-Right */}
+      <div className="col-span-2 ">
+        <div className="flex justify-end">
+          <img
+            src="/pages/rcnmap.svg"
+            className="max-w-[100%] md:max-w-[100%] lg:max-w-[100%] xl:max-w-[100%] h-[100vh] "
+          />
+        </div>
       </div>
     </div>
+    // <div className="w-full relative h-[100vh]">
+    //   <div className="w-full flex flex-col gap-10 px-[4vw] pt-[4vw]">
+    //     <div>
+    //       <p className="text-[#005FA6] text-[32px] font-[700]">RCN</p>
+    //     </div>
+    //     <div className="grid gap-6">
+    //       <p className="text-[#001424] text-[18px] font-[400]">
+    //         The RCN Cable was established <br /> through an agreement in 2010,
+    //         <br /> is a Terrestrial Cable that <br />
+    //         connects the United Arab
+    //         <br /> Emirates (UAE), Kingdom of
+    //         <br /> Saudi Arabia (KSA), Jordan, Syria,
+    //         <br /> and Turkey.
+    //       </p>
+    //       <p className="text-[#001424] text-[18px] font-[400]">
+    //         In 2015, the segments of the
+    //         <br /> cable in the UAE, KSA, and
+    //         <br /> Jordan were partially put into
+    //         <br /> service.
+    //       </p>
+    //     </div>
+    //   </div>
+    //   {/* Image Positioned at Bottom-Right */}
+    //   <div className="absolute top-0 right-0 h-[100vh]">
+    //     <img
+    //       src="/pages/rcnmap.svg"
+    //       className="max-w-[100%] md:max-w-[100%] lg:max-w-[100%] xl:max-w-[100%] h-[100vh] "
+    //     />
+    //   </div>
+    // </div>
   );
 };
 
